218ecb35b0d1d6f0d007f54116bd0f6e706abd83
LBU <lbu@LBUui-MacBook-Pro.local>
2021-01-05 12:40:46 -0500

    debugging POST

--- api/reviews/reviews-model.js
+++ api/reviews/reviews-model.js
@@ -5,15 +5,20 @@ module.exports = {
     get(){
         return db('trucks_reviews')
     },
-    getById(truck_id){
-        if(!truck_id){
+    getById(id){
+        if(!id){
             return Promise.resolve(null)
         }
         else{
-            return db('trucks_reviews').where({ truck_id }).first()
+            return db('trucks_reviews')
+            .where('truck_id', id)
+            .first()
         }
     },
-    async add(reviews){
+    add(reviews){
         return db('trucks_reviews').insert(reviews)
+            .then(([id]) => {
+                return db('trucks_reviews').where('truck_id', id).first()
+            })
     }
 }
