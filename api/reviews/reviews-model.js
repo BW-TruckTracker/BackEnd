21b155a4485ed19cb266bcbbb6da73ff08c3de93
Matt Canavan <mattcanavan@users.noreply.github.com>
2021-01-05 13:31:36 -0800

    Changed add() to be async.

--- api/reviews/reviews-model.js
+++ api/reviews/reviews-model.js
@@ -2,22 +2,26 @@ const db = require('../../database/db-config')
 
 
 module.exports = {
-    get(){
-        return db('trucks_reviews')
-    },
-    getById(id){
-        if(!id){
-            return Promise.resolve(null)
-        }
-        else{
-            return db('trucks_reviews')
-            .where('truck_id', id)
-        }
-    },
-    add(reviews){
-        return db('trucks_reviews').insert(reviews)
-            .then(([id]) => {
-                return db('trucks_reviews').where('truck_id', id)
-            })
+    get,
+    getById,
+    add,
+}
+
+function get(){
+    return db('trucks_reviews')
+}
+
+function getById(id){
+    if(!id){
+        return Promise.resolve(null)
     }
+    else{
+        return db('trucks_reviews')
+        .where('truck_id', id)
+    }
+}
+
+async function add(review){
+    const [id] = await db('trucks_reviews').insert(review);
+    return db('trucks_reviews').where('review_id', id)
 }
